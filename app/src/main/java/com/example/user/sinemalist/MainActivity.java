733068abbbbf1554696f319ae21d6d1a7d831e5e
slad_den <slad_den@mail.ru>
2018-05-05 15:26:05 +0300

    1.Создание списка фильмов ListView с тестовыми данными

---
+++ app/src/main/java/com/example/user/sinemalist/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.user.sinemalist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayList<String> arrayMovie=new ArrayList<>();
+        arrayMovie.add("Операция Ы");
+        arrayMovie.add("Терминатор");
+        arrayMovie.add("Рембо");
+
+        ArrayAdapter<String> adapter =new ArrayAdapter<>(this,R.layout.movie_list_item,R.id.textView,arrayMovie);
+
+        ListView listMovies= (ListView) findViewById(R.id.listMovies);
+        listMovies.setAdapter(adapter);
+
+    }
+}
